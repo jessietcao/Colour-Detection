c7f2cad80c134c4feef159133edeca2f58112146
Jessie Cao <63372882+jessietcao@users.noreply.github.com>
2024-05-12 23:04:02 -0400

    Update colour_detection.py

--- colour_detection.py
+++ colour_detection.py
@@ -17,17 +17,17 @@ clicked = False
 r = g = b = xpos = ypos = 0
 
 #Reading csv file with pandas and giving names to each column
-index=["color","color_name","hex","R","G","B"]
-csv = pd.read_csv('colors.csv', names=index, header=None)
+index=["colour","colour_name","hex","R","G","B"]
+csv = pd.read_csv('colours.csv', names=index, header=None)
 
-#function to calculate minimum distance from all colors and get the most matching color
-def getColorName(R,G,B):
+#function to calculate minimum distance from all colours and get the most matching colour
+def getColourName(R,G,B):
     minimum = 10000
     for i in range(len(csv)):
         d = abs(R- int(csv.loc[i,"R"])) + abs(G- int(csv.loc[i,"G"]))+ abs(B- int(csv.loc[i,"B"]))
         if(d<=minimum):
             minimum = d
-            cname = csv.loc[i,"color_name"]
+            cname = csv.loc[i,"colour_name"]
     return cname
 
 #function to get x,y coordinates of mouse double click
@@ -50,13 +50,13 @@ while(1):
     cv2.imshow("image",img)
     if (clicked):
    
-        #cv2.rectangle(image, startpoint, endpoint, color, thickness)-1 fills entire rectangle 
+        #cv2.rectangle(image, startpoint, endpoint, colour, thickness)-1 fills entire rectangle 
         cv2.rectangle(img,(20,20), (750,60), (b,g,r), -1)
 
-        #Creating text string to display( Color name and RGB values )
-        text = getColorName(r,g,b) + ' R='+ str(r) +  ' G='+ str(g) +  ' B='+ str(b)
+        #Creating text string to display( Colour name and RGB values )
+        text = getColourName(r,g,b) + ' R='+ str(r) +  ' G='+ str(g) +  ' B='+ str(b)
         
-        #cv2.putText(img,text,start,font(0-7),fontScale,color,thickness,lineType )
+        #cv2.putText(img,text,start,font(0-7),fontScale,colour,thickness,lineType )
         cv2.putText(img, text,(50,50),2,0.8,(255,255,255),2,cv2.LINE_AA)
 
         #For very light colours we will display text in black colour
@@ -79,8 +79,8 @@ img_path = args['image']
 img = cv2.imread(img_path)
 
 #Reading csv file with pandas and giving names to each column
-index=["color","color_name","hex","R","G","B"]
-csv = pd.read_csv('colors.csv', names=index, header=None)
+index=["colour","colour_name","hex","R","G","B"]
+csv = pd.read_csv('colours.csv', names=index, header=None)
 cv2.namedWindow('image')
 cv2.setMouseCallback('image',draw_function)
 def draw_function(event, x,y,flags,param):
@@ -97,13 +97,13 @@ def draw_function(event, x,y,flags,param):
 while(1):
     cv2.imshow("image",img)
     if (clicked):
-        #cv2.rectangle(image, startpoint, endpoint, color, thickness) -1 thickness fills rectangle entirely
+        #cv2.rectangle(image, startpoint, endpoint, colour, thickness) -1 thickness fills rectangle entirely
         cv2.rectangle(img,(20,20), (750,60), (b,g,r), -1)
 
-        #Creating text string to display ( Color name and RGB values )
-        text = getColorName(r,g,b) + ' R='+ str(r) + ' G='+ str(g) + ' B='+ str(b)
+        #Creating text string to display ( Colour name and RGB values )
+        text = getColourName(r,g,b) + ' R='+ str(r) + ' G='+ str(g) + ' B='+ str(b)
 
-        #cv2.putText(img,text,start,font(0-7), fontScale, color, thickness, lineType, (optional bottomLeft bool) )
+        #cv2.putText(img,text,start,font(0-7), fontScale, colour, thickness, lineType, (optional bottomLeft bool) )
         cv2.putText(img, text,(50,50),2,0.8,(255,255,255),2,cv2.LINE_AA)
   #For very light colours we will display text in black colour
         if(r+g+b>=600):
